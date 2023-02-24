bd472c8042b7bf8400f30943c8d3d8580f9c6bbe
Josh Elias <code.5jzcm@simplelogin.com>
2023-02-24 10:25:55 -0500

    Add support for streams Stop overriding file.hash Remove console.logs Remove extra comments

--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { Stream } from "stream";
-//import path from "path"; 
-//import { readFile } from "fs/promises";
 
-
-function sanitizeDirectory(directory: string) {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear().toString();
-    let month = (currentDate.getMonth() + 1).toString();
-    if(month.length === 1) {
-        month = "0" + month;
-    }
-
-    return (directory)
-        ? directory.replace(/(^\/)|(\/$)/g, "")
-        : `${year}/${month}`
-}
 
 export type SupabaseStorageProviderOptions = {
     apiUrl: string,
@@ -25,7 +10,34 @@ export type SupabaseStorageProviderOptions = {
     options?: Object
 }
 
-export default {
+export type StrapiFile = {
+    name: string
+    hash: string
+    ext: string
+    mime: string
+    path: object
+    width: number
+    height: number
+    size: number
+    url: string
+    buffer?: string
+    stream?: Stream
+    getStream: () => {}
+}
+
+async function streamToBuffer(stream: Stream): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+        const buffer: Buffer[] = [];
+        stream.on('data', (chunk) => buffer.push(chunk));
+        stream.on('end', () => {
+            resolve(Buffer.concat(buffer));
+        });
+        stream.on('error', reject);
+    });
+}
+
+
+module.exports = {
 
     init({
         apiUrl,
@@ -34,15 +46,18 @@ export default {
         directory = "",
 
     }: SupabaseStorageProviderOptions) {
-
         const supabase = createClient(apiUrl, apiKey);
-        directory = sanitizeDirectory(directory);
 
-        const upload = async (file: any, customParams = {}) => {
-            file.hash = new Date().getTime();
+        // Remove any leading and trailing slashes
+        directory = directory.replace(/(^\/)|(\/$)/g, "");
+
+        const upload = async (file: StrapiFile, customParams = {}) => {
             const path = (file.path) ? file.path : '';
             const uploadPath = `${directory}/${path}${file.hash}${file.ext}`;
-            const uploadData = file.stream || Buffer.from(file.buffer, 'binary');
+            const uploadData = (file.stream) 
+                ? await streamToBuffer(file.stream)
+                : Buffer.from(file.buffer!, 'binary');
+
             const { error } = await supabase
                 .storage
                 .from(bucket)
@@ -50,13 +65,13 @@ export default {
                     uploadPath, 
                     uploadData, 
                     {
-                        cacheControl: '3600',
+                        cacheControl: "3600",
                         upsert: false,
                         contentType: file.mime
                     }
                 );
             if(error) throw error;
-            
+   
             const { data: { publicUrl } } = supabase
                 .storage
                 .from(bucket)
@@ -79,56 +94,3 @@ export default {
         };
     },
 };
-
-
-
-
-
-
-// const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICAgInJvbGUiOiAic2VydmljZV9yb2xlIiwKICAgICJpc3MiOiAic3VwYWJhc2UiLAogICAgImlhdCI6IDE2NzYzNTA4MDAsCiAgICAiZXhwIjogMTgzNDExNzIwMAp9.3Ss_ANUkd1vznrNWctlWGXKuWtEy8Jf6Bj9U45uWfy4";
-// const API_URL = "http://localhost:8000";
-
-
-// const supabase = createClient(API_URL, API_KEY);
-
-// const currentDate = new Date();
-// const year = currentDate.getFullYear();
-// const month = currentDate.getMonth() + 1;
-
-// const bucket = "strapi-uploads";
-// const directory = `${year}/${month}`;
-// console.log("directory name: ", directory);
-
-
-// const upload2 = async (imagePath: string) => {
-//     const imageName = path.basename(imagePath);
-//     const imageData = await readFile(imagePath);
-//     const uploadPath = path.join(directory, imageName);
-//     const { data, error } = await supabase
-//         .storage
-//         .from(bucket)
-//         .upload(uploadPath, imageData, {
-//             cacheControl: '3600',
-//             upsert: false
-//         });
-//     if(error) {
-//         console.log(error.message);
-//         throw new Error("Error uploading to Supabase!");
-//     }
-
-//     console.log("Successfully Uploaded Image!");
-//     console.log(data);
-// }
-
-// function getFilePath(directory: string, file: any) {
-//     console.log("file", file);
-//     const path = (file.path) ? file.path : '';
-//     const fileName = file.name.replace(/\.[^/.]+$/, "");
-//     const uniqueFileName = `${path}${file.hash}${file.ext}`;
-//     return `${directory}/${uniqueFileName}`.replace(/^\//g, "");
-// }
-
-
-// const imagePath = path.join(__dirname, "test.jpg");
-// console.log("image path", imagePath);
-// upload(imagePath);
